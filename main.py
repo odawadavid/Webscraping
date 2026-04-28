920ea41b433374b6abea634aead72dff9e2f9c14
Odawa-Dev <odawa.dev@gmail.com>
2026-04-28 22:07:04 +0300

    update: added brands.py in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.scraping.naivas import run_naivas
 from src.scraping.quickmart import run_quickmart
 
 from src.processing.cleaning import run_cleaning
+from brands import KNOWN_BRANDS, COMMODITY_CATEGORIES
 from src.analysis.analysis import run_analysis
 
 
